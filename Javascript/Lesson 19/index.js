53c37e88c871c562a7d2dc507b45cc7083b8fbfc
Dhruvin Patel <pdhruvin221@gmail.com>
2020-10-02 16:29:28 -0500

    lesson 19 created

---
+++ Javascript/Lesson 19/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const MONTH_OF_BIRTH = "May";
+const FIRST_NAME = "Dhruvin";
+const LAST_NAME = "Patel";
+
+let city;
+let age;
+let nextCity;
+
+city = "Winnipeg";
+age = "26";
+
+nextCity = city;
+
+console.log("Month of birth is" , MONTH_OF_BIRTH);
+console.log("My first name is" , FIRST_NAME);
+console.log("My last name is" , LAST_NAME);
+console.log("I live in" , city);
+console.log("I am" , age , "years old");
+console.log("I want to stay in" , nextCity , "for life");
+console.log(`My full name is ${FIRST_NAME} ${LAST_NAME}`);
